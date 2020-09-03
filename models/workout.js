2247e9dc2c0b01d2aeed85496d55dcb3e9d9fcac
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-03 11:31:04 -0500

    updating models

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose =require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        excercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please, choose an excercise type."
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Please, enter an excercise name."
+                },
+                duration: {
+                    type: Number,
+                    required: "Please enter an excercise duration."
+                },
+                weight: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+);
