564144dd95d5c9a44eb544331a81d08cc36e8974
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-09-08 14:22:40 -0500

    updating routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongo = require("mongojs");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
@@ -10,13 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
